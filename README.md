bef44b6bf317a0b161aeb6de4869c7b71d16d61d
alangainie <alan.gainie@epitech.eu>
2025-08-31 04:12:31 +0200

    first commit no pro

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Little-Mario-Bros
 A littel Mario Bros game start and go with Yaouen and Alan
+
+Le but de se projet est de faire une sorte de petit mario bros afin de se remetre dans le bain du CSFML et de la création/ édition de jeux vidéo 2D rétro. Ce que nous allons essayer de réaliser :
+
+- Facile
+-> Mettre en place un background animer (parralaxe)
+-> Mettre en place les déplacements des personnages
+-> Gérer les sprites (croper)
+-> Mettre des sounds effect
+
+--Moyen
+-> Mettre en place un system de saut personnaliser (parrabolique)
+-> Faire une mini map
+-> Ajouter un item de pièce
+-> Ajouter un drapeau (condition de fin)
+
+--- Difficile
+-> Mettre en place un boutton cliquable afin de pouvoir sauvegarder la progression
+-> Rajoutter des options
+-> Faire des chargements de niveaux personnaliser
+
+---- Hardcore
+-> Ajouter des énemie
---
+++ windows.c
@@ -0,0 +1,46 @@
+/*
+** Exemple minimal : Création d'une fenêtre CSFML
+*/
+
+// Rapelle : gcc windows.c -o windows -lcsfml-graphics -lcsfml-window -lcsfml-system ./windows
+
+#include <SFML/Graphics.h>
+#include <SFML/Window.h>
+
+int main(void)
+{
+    // Définition de la taille et des paramètres de la fenêtre
+    sfVideoMode mode = {800, 600, 32};
+
+    // Création de la fenêtre
+    sfRenderWindow *window = sfRenderWindow_create(mode,
+                                                   "Fenetre CSFML",
+                                                   sfResize | sfClose,
+                                                   NULL);
+
+    // Limite de FPS
+    sfRenderWindow_setFramerateLimit(window, 60);
+
+    // Boucle principale
+    while (sfRenderWindow_isOpen(window))
+    {
+        sfEvent event;
+
+        // Gestion des événements
+        while (sfRenderWindow_pollEvent(window, &event))
+        {
+            if (event.type == sfEvtClosed)
+                sfRenderWindow_close(window);
+        }
+
+        // Effacer la fenêtre avec une couleur noire
+        sfRenderWindow_clear(window, sfBlack);
+
+        // Afficher le contenu
+        sfRenderWindow_display(window);
+    }
+
+    // Détruire la fenêtre à la fin
+    sfRenderWindow_destroy(window);
+    return 0;
+}
