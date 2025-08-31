4ade9cd5db071ab0a5ce782605b3a3f9b51f64e0
tudwallLD <yaouen.ledanvic@laposte.net>
2025-08-31 18:53:24 +0200

    Modified->setsprite add->setscale

--- src/setsprite.c
+++ src/setsprite.c
@@ -1,12 +1,13 @@
 #include "../include/my.h"
 
 // Ma fonction créer un sprite
-sfSprite *createSprite(sfVector2f possition, char *img)
+sfSprite *createSprite(sfVector2f setPossition, char *img, sfVector2f setScale)
 {
     sfSprite *constructsprite = sfSprite_create();
     sfTexture *constructspritetexture = sfTexture_createFromFile(img, NULL);
 
     sfSprite_setTexture(constructsprite, constructspritetexture, sfTrue);
-    sfSprite_setPosition(constructsprite, possition);
+    sfSprite_setPosition(constructsprite, setPossition);
+    sfSprite_setScale(img, setScale);
     return constructsprite;
 }
