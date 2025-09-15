0fccb49029a3dc094dc911d0c8d45a99f98b14fe
tudwallLD <yaouen.ledanvic@laposte.net>
2025-09-15 02:47:35 +0200

    Update setsprite.c

--- src/setsprite.c
+++ src/setsprite.c
@@ -7,7 +7,6 @@ sfSprite *createSprite(sfVector2f setPossition, char *img, sfVector2f setScale, 
     constructsprite = sfSprite_create();
     sfTexture *constructspritetexture = sfTexture_createFromFile(img, NULL);
 
-    
     sfSprite_setTexture(constructsprite, constructspritetexture, sfTrue);
     sfSprite_setPosition(constructsprite, setPossition);
     sfSprite_setScale(constructsprite, setScale);
